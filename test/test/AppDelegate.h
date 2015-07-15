3ce45c57db050053124f2d16a8e4e3d8a8bf0444
poorfish <poorfish@gmail.com>
2015-07-15 23:18:30 +0800

    first commit

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by poorfish on 15/7/15.
+//  Copyright (c) 2015年 poorfish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by poorfish on 15/7/15.
+//  Copyright (c) 2015年 poorfish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
